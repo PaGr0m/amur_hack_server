8d866d3e61d301da0ca668d1dbb9a12327ebef26
Pavel Gromov <pagrom27@gmail.com>
2018-10-13 12:14:55 +1000

    Redact models
    Relations

--- main/models.py
+++ main/models.py
@@ -16,6 +16,32 @@ class User(models.Model):
         return self.login
 
 
+# Абсолютно не уверен в данном классе. Его делаю, чтобы урны могли
+# ссылаться на {типы}. Возможно это все решается простым {enum}
+class TypesUrn:
+    name = models.TextField(verbose_name="Тип мусора")
+
+    class Meta:
+        verbose_name = "Тип мусорки"
+        verbose_name_plural = "Типы мусорок"
+
+    def __str__(self):
+        return self.name
+
+
+class Location(models.Model):
+    name = models.TextField(verbose_name="Название места")
+    coordinate_x = models.TextField(verbose_name="Долгота")
+    coordinate_y = models.TextField(verbose_name="Широта")
+
+    class Meta:
+        verbose_name = "Местоположение"
+        verbose_name_plural = "Местоположения"
+
+    def __str__(self):
+        return self.name
+
+
 class Urn(models.Model):
     location = models.ForeignKey("Location",
                                  verbose_name="Местоположение",
@@ -34,27 +60,6 @@ class Urn(models.Model):
         return str(self.location) + " " + str(self.id)
 
 
-class Location(models.Model):
-    name = models.TextField(verbose_name="Название места")
-    coordinate_x = models.TextField(verbose_name="Долгота")
-    coordinate_y = models.TextField(verbose_name="Широта")
-
-    class Meta:
-        verbose_name = "Местоположение"
-        verbose_name_plural = "Местоположения"
-
-    def __str__(self):
-        return self.name
 
 
-# Абсолютно не уверен в данном классе. Его делаю, чтобы урны могли
-# ссылаться на {типы}. Возможно это все решается простым {enum}
-class TypesUrn:
-    name = models.TextField(verbose_name="Тип мусора")
 
-    class Meta:
-        verbose_name = "Тип мусорки"
-        verbose_name_plural = "Типы мусорок"
-
-    def __str__(self):
-        return self.name
