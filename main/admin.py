cfe26d2dd566da16ec8cc5af9e7c7088e14e2dfb
Pavel Gromov <pagrom27@gmail.com>
2018-10-13 12:32:55 +1000

    Admin panel

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Location, Urn, TypesUrn, User
 
-# Register your models here.
+admin.site.register(Location)
+admin.site.register(Urn)
+admin.site.register(TypesUrn)
+admin.site.register(User)
