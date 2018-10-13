19fb9001b02c70f9579221a4b91cc3a36c571086
fixen94 <fixen94@gmail.com>
2018-10-13 21:55:07 +1000

    То что хотел Ваня с минимум потерь

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import Location, Urn, Client
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 admin.site.register(Location)
@@ -9,15 +9,19 @@ class UrnAdmin(admin.ModelAdmin):
 
 admin.site.register(Urn, UrnAdmin)
 
-class ClientInline(admin.StackedInline):
-    model = Client
-    can_delete = False
-    verbose_name_plural = 'пользователи'
+# class ClientInline(admin.StackedInline):
+#     model = Client
+#     can_delete = False
+#     verbose_name_plural = 'пользователи'
+#
+# # Define a new User admin
+# class UserAdmin(BaseUserAdmin):
+#     inlines = (ClientInline,)
+#
+# # Re-register UserAdmin
+# admin.site.unregister(User)
+# admin.site.register(User, UserAdmin)
 
-# Define a new User admin
-class UserAdmin(BaseUserAdmin):
-    inlines = (ClientInline,)
-
-# Re-register UserAdmin
 admin.site.unregister(User)
-admin.site.register(User, UserAdmin)
+admin.site.unregister(Group)
+admin.site.register(Client)
